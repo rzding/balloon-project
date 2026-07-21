d7e50e6dc93a0675f05d16ad08879365b0ddf5f5
Avaneesh02 <pavaneesh14@gmail.com>
2026-07-20 22:17:41 -0700

    Code Generation from STM32MX (Starting point for code)

---
+++ balloon-project-stm32mx/Core/Inc/main.h
@@ -0,0 +1,99 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define APRS_PWM_Pin GPIO_PIN_0
+#define APRS_PWM_GPIO_Port GPIOA
+#define Cam_CS_Pin GPIO_PIN_4
+#define Cam_CS_GPIO_Port GPIOA
+#define IMU_CS_Pin GPIO_PIN_0
+#define IMU_CS_GPIO_Port GPIOB
+#define LoRa_CS_Pin GPIO_PIN_1
+#define LoRa_CS_GPIO_Port GPIOB
+#define BARO_CS_Pin GPIO_PIN_2
+#define BARO_CS_GPIO_Port GPIOB
+#define LoRa_DIO0_Pin GPIO_PIN_12
+#define LoRa_DIO0_GPIO_Port GPIOB
+#define LoRa_DIO0_EXTI_IRQn EXTI15_10_IRQn
+#define LoRa_RESET_Pin GPIO_PIN_13
+#define LoRa_RESET_GPIO_Port GPIOB
+#define APRS_PTT_Pin GPIO_PIN_14
+#define APRS_PTT_GPIO_Port GPIOB
+#define APRS_PD_Pin GPIO_PIN_15
+#define APRS_PD_GPIO_Port GPIOB
+#define Temp_CS_Pin GPIO_PIN_8
+#define Temp_CS_GPIO_Port GPIOA
+#define IMU_INT1_Pin GPIO_PIN_15
+#define IMU_INT1_GPIO_Port GPIOA
+#define IMU_INT1_EXTI_IRQn EXTI15_10_IRQn
+#define microSD_CS_Pin GPIO_PIN_3
+#define microSD_CS_GPIO_Port GPIOB
+#define microSD_detect_Pin GPIO_PIN_4
+#define microSD_detect_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
